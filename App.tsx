552442ccb09e535c75cb718448807456299a3b92
sunnymaurya719 <sunnymaurya1920@gmail.com>
2024-08-22 20:16:19 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View style={{justifyContent:'center',alignItems:'center'}}>
-      <Text style={{color:'yellow'}}>superr ndsosndaodaok</Text>
+      <View style={{ flex:1,backgroundColor: '#1A1A1A' }}>
+      <Text 
+      style={{
+        color: '#FFFFF',
+        fontSize: 20,
+        fontWeight: '700',
+        marginTop: 30,
+        marginLeft: 20,
+      }}>
+        Book App
+      </Text>
+
+
+      <View style={{marginTop:20}}>
+      <FlatList
+          data={[1, 1, 1, 1, 1, 1]}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item, index }) => {
+            return (
+              <TouchableOpacity
+              style={{
+                width: 120,
+                height: 120,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: '#fff',
+                marginLeft: 20,
+              }}></TouchableOpacity>
+            );
+          }}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default App
 
